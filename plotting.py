537b97bbb1f4733146b203a7520fac607c802731
YthanZhang <cn.yiki@gmail.com>
2020-08-02 11:00:01 +0800

    add plotting.plot_complex3d()

--- plotting.py
+++ plotting.py
@@ -1,16 +1,27 @@
 import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d
 
 
-def draw(x: list, y: list) -> None:
-    plt.plot(x, y)
-    plt.draw()
+def plot(numbers) -> None:
+    plt.figure()
+    plt.plot(numbers)
+
+
+def plot_complex3d(numbers: list):
+    real = [number.real for number in numbers]
+    imag = [number.imag for number in numbers]
+    time = [n for n in range(len(numbers))]
+
+    plt.figure()
+    f = plt.gca(projection="3d")
+    f.plot(time, real, imag)
 
 
 def plot_complex(numbers: list, xlim=(-2, 2), ylim=(-2, 2)):
     real = [number.real for number in numbers]
     imag = [number.imag for number in numbers]
 
-    f = plt.figure()
+    plt.figure()
     plt.plot(real, imag)
     plt.xlim(xlim)
     plt.ylim(ylim)
